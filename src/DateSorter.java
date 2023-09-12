0c1c5b46e31edbdc2a31463e473b978491267d37
dpointtt <64966626+dpointtt@users.noreply.github.com>
2023-09-12 05:50:16 +0300

    TEST_TASK_002:EXTRA:Adding utility function

--- src/DateSorter.java
+++ src/DateSorter.java
@@ -7,4 +7,11 @@ public class DateSorter {
     public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
         return new ArrayList<>();
     }
+
+    // функц≥€ дл€ перев≥рки дати на букву "r"
+    private boolean hasRInMonth(LocalDate date){
+        int month = date.getMonthValue();
+        // м≥с€ц≥ з 5 по 8 не мають букви
+        return (month < 5 || month > 8);
+    }
 }
