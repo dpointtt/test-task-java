67b5a7faf5422c1c56f3938890c4ec93f9a1da0e
dpointtt <64966626+dpointtt@users.noreply.github.com>
2023-09-12 05:45:54 +0300

    TEST_TASK_001:DEPLOY:Adding required class

---
+++ src/DateSorter.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class DateSorter {
+    public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
+        return new ArrayList<>();
+    }
+}
